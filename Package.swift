2f899131e4bba0f85712c6c2cf6da6bfae6a4492
Stanislav <stanistad@gmail.com>
2022-12-18 14:57:27 +0100

    support swift package manager

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version: 5.7.0
+import PackageDescription
+
+let package = Package(
+    name: "KRActivityIndicatorView",
+    products: [
+        .library(
+            name: "KRActivityIndicatorView",
+            targets: ["KRActivityIndicatorView"]
+        )
+    ],
+    targets: [
+        .target(name: "KRActivityIndicatorView")
+    ]
+)
